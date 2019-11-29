c3febe50b3cd36b470cadf8a692c367a843497bb
fwfichtner <fwfichtner@gmail.com>
2019-11-29 16:33:17 +0100

    added first models and listed them on the website, created superuser

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)  # Regular & Sicilian
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Size(models.Model):
+    size = models.IntegerField()  # Cheese=0, 1-3, Special=5
+
+    def __str__(self):
+        return f"{self.size}"
+
+
+class Pizza(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="pizza_category")
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name="pizza_size")  # TODO bad naming choice
+    small = models.DecimalField(max_digits=4, decimal_places=2)  # price in $
+    large = models.DecimalField(max_digits=4, decimal_places=2)  # price in $
+
+    def __str__(self):
+        topping = f"Cheese" if f"{self.size}" == "0" else f"{self.size} toppings"  # TODO there must be a better way
+        return f"{self.category} : {topping} - small: {self.small} $ // large: {self.large} $"
+
+
+class Toppings(models.Model):
+    topping = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.topping}"
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Pizza, Toppings
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    context = {
+        "pizzas": Pizza.objects.all(),
+        "toppings": Toppings.objects.all()
+    }
+    return render(request, "orders/index.html", context)
+
